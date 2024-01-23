b5c164d1f12cc666ccf8ff8f3479a9397783d1d2
YevhenKustovskiy <136888021+YevhenKustovskiy@users.noreply.github.com>
2024-01-23 17:54:49 +0200

    Add files via upload

---
+++ geometries/atoms/fluorine_geometry.py
@@ -0,0 +1,12 @@
+from geometries.atoms.base_atom_geometry import BaseAtomGeometry
+
+
+class FluorineGeometry(BaseAtomGeometry):
+    def __new__(cls):
+        if not hasattr(cls, "instance"):
+            cls.instance = super(FluorineGeometry, cls).__new__(cls)
+        return cls.instance
+
+
+    def __init__(self):
+        BaseAtomGeometry.__init__(self, radius = 0.31)
